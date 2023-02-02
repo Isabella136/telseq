f3d1241da1a50068c96d7cfacd67286f079c480f
Isabella136 <nbonin@ufl.edu>
2023-02-02 09:42:58 -0500

    Removed merge_overlap

--- run.sh
+++ run.sh
@@ -6,8 +6,8 @@
 #SBATCH --mail-user=nbonin@ufl.edu
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=64gb
-#SBATCH --time=120:00:00
+#SBATCH --mem=2gb
+#SBATCH --time=1:00:00
 #SBATCH --output=logs/%j_disp.log
 #SBATCH --error=logs/%j_disp.log
 
@@ -24,9 +24,9 @@ module load snakemake
 ##----------------------------------------------------------
 # Run
 
-snakemake --touch --forceall -j 1000 --rerun-incomplete 
+#snakemake --touch --forceall -j 10000 --rerun-incomplete 
 
 snakemake --cluster "sbatch -A {cluster.account} -q {cluster.qos} -c {cluster.cpus-per-task} -N {cluster.Nodes} \
   -t {cluster.runtime} --mem {cluster.mem} -J {cluster.jobname} --mail-type={cluster.mail_type} \
   --mail-user={cluster.mail} --output {cluster.out} --error {cluster.err}" \
-  --cluster-config config/cluster.json --jobs 300 --latency-wait 20 --rerun-incomplete --use-envmodules --use-conda --conda-frontend mamba
+  --cluster-config config/cluster.json --jobs 500 --rerun-trigger mtime --latency-wait 20  --use-envmodules --use-conda --conda-frontend mamba
